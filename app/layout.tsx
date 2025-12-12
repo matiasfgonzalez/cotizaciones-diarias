885205114e332a27702e01b169471ed7707abcae
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2025-12-11 21:07:44 -0300

    Agregue Vercel Analytics para obtener mejores conocimientos de seguimiento y rendimiento

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -129,6 +130,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
